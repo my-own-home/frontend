7841f61cb87ceab168fa5029ee27c0c8a483ef3a
sujung <hsj8518@gmail.com>
2022-11-16 02:05:57 +0900

    add notice

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,7 @@ import ElProgressBars from "../layouts/sections/elements/progress-bars/ProgressB
 import ElToggles from "../layouts/sections/elements/toggles/TogglesView.vue";
 import ElTypography from "../layouts/sections/elements/typography/TypographyView.vue";
 import LoginView from "@/views/AppLogin.vue";
+import NoticeView from "@/views/AppNotice.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -36,6 +37,11 @@ const router = createRouter({
       path: "/login",
       name: "login",
       component: LoginView,
+    },
+    {
+      path: "/notice",
+      name: "notice",
+      component: NoticeView,
     },
     {
       path: "/pages/landing-pages/about-us",
