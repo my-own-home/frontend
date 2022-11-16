4f0a8cc2c404a1e9cfa23b51255604c50fcf9d5c
sujung <hsj8518@gmail.com>
2022-11-16 18:01:58 +0900

    add userpage

---
+++ src/views/AppUser.vue
@@ -0,0 +1,90 @@
+<script setup>
+// example components
+import DefaultNavbar from "@/examples/navbars/NavbarDefault.vue";
+import Header from "@/examples/Header.vue";
+
+// sections
+import Footer from "@/components/Author/AuthorFooter.vue";
+import Profile from "@/components/Author/AuthorProfile.vue";
+import Posts from "@/components/Author/AuthorPosts.vue";
+import Contact from "@/components/Author/AuthorContact.vue";
+
+// image
+import image from "@/assets/img/city-profile.jpg";
+</script>
+<template>
+  <DefaultNavbar transparent />
+  <Header>
+    <div
+      class="page-header min-height-200"
+      :style="{ backgroundImage: `url(${image})` }"
+      loading="lazy"
+    >
+      <span class="mask bg-gradient-dark opacity-8"></span>
+    </div>
+  </Header>
+  <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6 mb-4">
+    <Profile />
+    <div class="container">
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link active"
+            id="home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#home"
+            type="button"
+            role="tab"
+            aria-controls="home"
+            aria-selected="true"
+          >
+            개인정보수정
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#profile"
+            type="button"
+            role="tab"
+            aria-controls="profile"
+            aria-selected="false"
+          >
+            관심지역
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="contact-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#contact"
+            type="button"
+            role="tab"
+            aria-controls="contact"
+            aria-selected="false"
+          >
+            관심매물
+          </button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+          ...
+        </div>
+        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+          ...
+        </div>
+        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+          ...
+        </div>
+      </div>
+    </div>
+    <Posts />
+  </div>
+
+  <Contact />
+  <Footer />
+</template>
