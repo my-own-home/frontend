3708e69ba83aef4af51e206222bda915793d180b
sujunghwang <64738942+sujunghwang@users.noreply.github.com>
2022-11-16 20:55:26 +0900

    add userpage (#6)
    add userpage

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,8 @@ import ElDropdowns from "../layouts/sections/elements/dropdowns/DropdownsView.vu
 import ElProgressBars from "../layouts/sections/elements/progress-bars/ProgressBarsView.vue";
 import ElToggles from "../layouts/sections/elements/toggles/TogglesView.vue";
 import ElTypography from "../layouts/sections/elements/typography/TypographyView.vue";
+
+import UserView from "@/views/AppUser.vue";
 import LoginView from "@/views/AppLogin.vue";
 import NoticeView from "@/views/AppNotice.vue";
 import LandingView from "@/views/AppLanding.vue";
@@ -34,6 +36,11 @@ const router = createRouter({
       path: "/",
       name: "presentation",
       component: PresentationView,
+    },
+    {
+      path: "/user",
+      name: "user",
+      component: UserView,
     },
     {
       path: "/login",
---
+++ src/views/AppLanding.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <navbar-simple sticky="true"></navbar-simple>
+    <header class="bg-gradient-dark">
+      <landing-bg-video src="vid/apartments.mp4"></landing-bg-video>
+      <span class="mask bg-gradient-light opacity-3"></span>
+      <div class="container">
+        <div class="row justify-content-center">
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+import bgVideo from "@/assets/vid/apartments.mp4";
+
+import NavbarSimple from "@/components/common/NavbarSimple.vue";
+import LandingBgVideo from "@/components/landing/LandingBgVideo.vue";
+</script>
+
+<style scoped>
+.container {
+  z-index: 2;
+  padding-top: 12vh;
+  padding-bottom: 40px;
+}
+</style>
