3530c77b4198b068d3f8d4c82d937305179a7129
teamzamong <77232235+teamzamong@users.noreply.github.com>
2022-11-17 08:51:56 +0900

    add: Q&A 페이지 accordion에 화살표 추가 (#9)

--- src/views/AppQna.vue
+++ src/views/AppQna.vue
@@ -193,6 +193,7 @@ export default {
 <style>
 h2.accordion-header > button {
   padding-left: 12px !important;
+  padding-right: 12px !important;
   border-bottom: 1px solid rgba(210, 210, 210, 1) !important;
 }
 h2.accordion-header > button:hover {
@@ -200,5 +201,8 @@ h2.accordion-header > button:hover {
 }
 div.accordion-body {
   border-bottom: 1px solid rgba(210, 210, 210, 1) !important;
+}
+.accordion-button:after{
+  background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23344767'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>") !important;
 }
 </style>
